1661642709e93aaae431600e4daff10866abc459
aya zartit <ayakakaa135@gmail.com>
2026-02-04 18:45:26 +0100

    Update email settings for SMTP configuration

--- config/settings.py
+++ config/settings.py
@@ -177,12 +177,13 @@ ACCOUNT_LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 
 DEFAULT_FROM_EMAIL = 'kaaka310690@gmail.com'
 
