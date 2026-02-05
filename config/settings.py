effc642e8c5d698940a2bb35d0b5161598f6f27d
ayakakaa135-boop <241167334+ayakakaa135-boop@users.noreply.github.com>
2026-02-05 04:33:31 -0500

    Fix: Switch to SMTP port 465 with SSL to resolve timeout issue

--- config/settings.py
+++ config/settings.py
@@ -177,8 +177,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_TIMEOUT = 10  # مهلة الاتصال بخادم البريد لتجنب تعليق الطلب
 
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
